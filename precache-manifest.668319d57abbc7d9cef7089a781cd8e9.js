4a5b97ce779066338b805e21edae0df2fc378c15
github username <github email address>
2019-05-08 15:25:26 -0700

    Updates

---
+++ precache-manifest.668319d57abbc7d9cef7089a781cd8e9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee941a276f34c68efd8a6ff18488f11",
+    "url": "/frontend_admin_metro_pizza/index.html"
+  },
+  {
+    "revision": "cede6b8d9ecceb44dfbb",
+    "url": "/frontend_admin_metro_pizza/static/css/2.f80f2b8a.chunk.css"
+  },
+  {
+    "revision": "3b5506460717ae6136a2",
+    "url": "/frontend_admin_metro_pizza/static/css/main.d941d65d.chunk.css"
+  },
+  {
+    "revision": "cede6b8d9ecceb44dfbb",
+    "url": "/frontend_admin_metro_pizza/static/js/2.e8b7b5da.chunk.js"
+  },
+  {
+    "revision": "3b5506460717ae6136a2",
+    "url": "/frontend_admin_metro_pizza/static/js/main.b48a145d.chunk.js"
+  },
+  {
+    "revision": "bc3dc5123864510cb42d",
+    "url": "/frontend_admin_metro_pizza/static/js/runtime~main.1ba7b0f3.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/frontend_admin_metro_pizza/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/frontend_admin_metro_pizza/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/frontend_admin_metro_pizza/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/frontend_admin_metro_pizza/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/frontend_admin_metro_pizza/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "f6a00497cd37a2d76a285efaddb34fc4",
+    "url": "/frontend_admin_metro_pizza/static/media/elf-icon.f6a00497.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/frontend_admin_metro_pizza/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/frontend_admin_metro_pizza/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/frontend_admin_metro_pizza/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/frontend_admin_metro_pizza/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/frontend_admin_metro_pizza/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/frontend_admin_metro_pizza/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/frontend_admin_metro_pizza/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/frontend_admin_metro_pizza/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/frontend_admin_metro_pizza/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/frontend_admin_metro_pizza/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/frontend_admin_metro_pizza/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
